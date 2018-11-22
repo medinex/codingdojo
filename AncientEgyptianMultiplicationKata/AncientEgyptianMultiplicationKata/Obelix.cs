cb4c2ca01c61aec0b66f6b19289fcccec4d8e6f5
benjamin.weiss <benjamin.weiss@nexus-ag.de>
2018-11-22 11:33:41 +0100

    Adds AncientEgyptianMultiplicationKata

---
+++ AncientEgyptianMultiplicationKata/AncientEgyptianMultiplicationKata/Obelix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientEgyptianMultiplicationKata
+{
+    public class Obelix
+    {
+        public object Mul(int a, int b)
+        {
+            if (a < 1 || b < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            int x, y, res;
+            x = a;
+            y = b;
+            res = 0;
+            while (x > 0)
+            {
+                if (x % 2 != 0)
+                {
+                    res += y;
+                }
+
+                x = x / 2;
+                y = y * 2;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ AncientEgyptianMultiplicationKata/NUnit.Tests1/TestClass.cs
@@ -0,0 +1,47 @@
+﻿using AncientEgyptianMultiplicationKata;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase(33, 78, 2574)]
+        [TestCase(47, 42, 1974)]
+        [TestCase(3, 4, 12)]
+        [TestCase(1, 1, 1)]
+        public void TestMethod(int x, int y, int result)
+        {
+            var obelix = new Obelix();
+            var r = obelix.Mul(x, y);
+            Assert.That(r, Is.EqualTo(result));
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        [TestCase(-1, 1)]
+        [TestCase(1, -1)]
+        public void InvadlidNumber(int x, int y)
+        {
+            var obelix = new Obelix();
+            Assert.That(() => obelix.Mul(x, y), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void MultipleCalculations()
+        {
+            var obelix = new Obelix();
+            for (int i = 1; i < 999; i++)
+                for (int j = 1; j < 999; j++)
+                    Assert.That(() => obelix.Mul(i, j), Is.EqualTo(i * j));
+        }
+
+
+    }
+}
