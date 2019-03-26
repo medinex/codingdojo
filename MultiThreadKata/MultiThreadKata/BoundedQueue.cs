c14cb5f14dd0a083f84c243b25321b75441a68fb
benjamin.weiss <benjamin.weiss@nexus-ag.de>
2019-03-26 08:54:42 +0100

    Adds solution and first implementation for Threading-Kata

---
+++ MultiThreadKata/MultiThreadKata/BoundedQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadKata
+{
+    public class BoundedQueue<T>
+    {
+        private T[] _list ;
+        private readonly object _door = new object();
+
+        public int Size { get; set; }
+        public int Count { get; set; }
+
+        public BoundedQueue(int size)
+        {
+            _list = new T[size];
+            Size = size;
+        }
+
+        public void Enqueue(T element)
+        {
+            lock (_door)
+            {
+                while (Count >= Size)
+                {
+                    Thread.Sleep(10);                    
+                }
+
+                _list[Count++] = element;
+            }
+        }
+
+        public T Dequeue()
+        {
+            var retVal = _list[0];
+            for (int i = 1; i < Count; i++)
+                _list[i - 1] = _list[i];
+            Count--;
+            return retVal;
+        }
+
+
+    }
+}
---
+++ MultiThreadKata/NUnit.Tests1/TestClass.cs
@@ -0,0 +1,81 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MultiThreadKata;
+using NUnit.Framework;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void IfEnqueueIsEqualDequeue()
+        {
+            var queue = new BoundedQueue<int>(2);
+
+            queue.Enqueue(1);
+
+            Assert.That(queue.Dequeue(), Is.EqualTo(1), "Some useful error message");
+        }
+
+        [Test]
+        public void IfEnqueueIsEqualDequeueMultiple()
+        {
+            var queue = new BoundedQueue<int>(2);
+
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Dequeue();
+
+            Assert.That(queue.Dequeue(), Is.EqualTo(3), "Some useful error message");
+        }
+
+        [Test]
+        public void IfEnqueueIsEqualDequeueTwice()
+        {
+            var queue = new BoundedQueue<int>(2);
+
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Assert.That(queue.Dequeue(), Is.EqualTo(2), "Some useful error message");
+        }
+
+
+        [Test]
+        public void Enqueue3Dequeue1()
+        {
+            var queue = new BoundedQueue<int>(2);
+
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            var t = Task.Run(() => queue.Enqueue(4));
+            var timeout = Task.WhenAny(t, Task.Delay(100));
+           
+            Assert.That(t.IsCompleted, Is.False, "Some useful error message");
+        }
+
+        [Test]
+        public async Task EnqueueAndDequeueMultiThreaded()
+        {
+            var queue = new BoundedQueue<int>(2);
+
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            var t = Task.Run(() => queue.Enqueue(4));
+            var timeout = Task.WhenAny(t, Task.Delay(100));
+            await timeout;
+            Assert.That(t.IsCompleted, Is.False, "Some useful error message");
+
+            queue.Dequeue();
+            await t;
+
+            Assert.That(t.IsCompleted, Is.True, "Some useful error message");
+            Assert.That(queue.Count, Is.EqualTo(2), "Some useful error message");
+        }
+
+    }
+}
