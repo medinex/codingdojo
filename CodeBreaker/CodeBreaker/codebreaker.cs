6b174bd1ac91f294dbc6f51b7caf3d32cb20bc7f
benjamin.weiss <benjamin.weiss@nexus-ag.de>
2019-06-18 09:04:30 +0200

    Adds new kata

---
+++ CodeBreaker/CodeBreaker/codebreaker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBreaker
+{
+    public class Enigma
+    {
+        private static string _alphabet = "abcdefghijklmnopqrstuvwxyz ";
+        private static string _key = "!)\"(£*%&><@abcdefghijklmno ";
+
+        public static string Decrypt(string message)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var c in message)
+            {
+                var pos = _alphabet.First(x => x == c);
+                sb.Append(_key[pos]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CodeBreaker/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CodeBreaker;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow("hello world", "&£aad ldga(")]
+        [DataRow("super lopes", "hje£g ade£h")]
+        [DataRow("nexus ag", "c£mjh !%")]
+        public void TestDecryption(string message, string encrypt)
+        {
+            // a b c d e f g h i j k l m n o p q r s t u v w x y z
+            // ! ) " ( £ * % & > < @ a b c d e f g h i j k l m n o
+
+            var encryptedMEssage = Enigma.Decrypt(encrypt);
+
+            Assert.AreEqual(  message , encryptedMEssage);
+
+
+
+        }
+    }
+}
