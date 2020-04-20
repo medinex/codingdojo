150ad36eeb1f051fbbd2df2af331f73c1bc85ec2
medinex <benjamin.weiss@nexus-ag.de>
2020-04-20 19:21:36 +0200

    adds simple process monitor to track start/stop of processes based on event polling

---
+++ ProcessMonitor/ProcessMonitor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Management;
+
+namespace ProcessMonitor
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ManagementEventWatcher startWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+
+            startWatch.EventArrived += new EventArrivedEventHandler(startWatch_EventArrived);
+            startWatch.Start();
+            
+            ManagementEventWatcher stopWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace"));
+            
+            stopWatch.EventArrived += new EventArrivedEventHandler(stopWatch_EventArrived);
+            stopWatch.Start();
+            
+            Console.WriteLine("Press any key to exit");
+            
+            while (!Console.KeyAvailable) System.Threading.Thread.Sleep(50);
+            
+            startWatch.Stop();
+            stopWatch.Stop();
+        }
+
+        static void stopWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Process stopped: {0}", e.NewEvent.Properties["ProcessName"].Value);
+        }
+
+        static void startWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Process started: {0}", e.NewEvent.Properties["ProcessName"].Value);
+        }
+    }
+}
