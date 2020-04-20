2d859d7aeb8f77d11a5a6a04ed8903234018fa37
medinex <benjamin.weiss@nexus-ag.de>
2020-04-20 20:10:02 +0200

    makes it all un-static :)

---
+++ ProcessMonitor/ProcessMonitor/ProcessMonitor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+using System.Text;
+using System.Threading;
+
+namespace ProcessMonitor
+{
+    public class PerformanceMonitor
+    {
+        private string _comment;
+        private ManagementEventWatcher _start;
+        private ManagementEventWatcher _stop;
+
+        public PerformanceMonitor()
+        {
+            _start = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            _stop = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+
+        }
+
+        public void start()
+        {
+            startMonitoringOn(_start, "started");
+            startMonitoringOn(_stop, "stopped");
+        }
+
+        public void stop()
+        {
+            stopMonitoringOn(_start);
+            stopMonitoringOn(_stop);
+        }
+
+        private void startMonitoringOn(ManagementEventWatcher eventWatcher, string comment)
+        {
+            _comment = comment;
+            eventWatcher.EventArrived += new EventArrivedEventHandler(reportOnConsole);
+            eventWatcher.Start();
+        }
+
+        private void stopMonitoringOn(ManagementEventWatcher eventWatcher)
+        {
+            eventWatcher.Stop();
+        }
+
+        private void reportOnConsole(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Process {0}: {1}", _comment, e.NewEvent.Properties["ProcessName"].Value);
+        }
+    }
+}
