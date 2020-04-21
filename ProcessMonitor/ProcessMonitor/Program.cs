55d4b74452e0cba77cfb4a770981a1d3c4427b18
medinex <43470488+medinex@users.noreply.github.com>
2020-04-21 08:05:42 +0200

    Procmon Integration (#9)
    * adds blank line for test purposes
    
    * adds simple process monitor to track start/stop of processes based on event polling
    
    * makes it all un-static :)

---
+++ ProcessMonitor/ProcessMonitor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProcessMonitor
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            PerformanceMonitor pm = new PerformanceMonitor();
+            pm.start();
+
+            Console.WriteLine("Press any key to exit");
+            while (!Console.KeyAvailable) System.Threading.Thread.Sleep(50);
+
+            pm.stop();
+        }
+    }
+}
