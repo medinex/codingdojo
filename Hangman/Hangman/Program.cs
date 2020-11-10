c3b03be5a7b7e6ad12ed4f21ed44952036e763e5
jakub.sienkiewicz@nexus-ag.de <jakub.sienkiewicz@nexus-ag.de>
2020-11-10 08:51:23 +0100

    UT Hangman

---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HangmanGame
+{
+        public class Hangman
+    {
+        public Hangman(string secret) 
+        { 
+        }
+        public string Guess(char letter) {
+            return "";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Hangman/HangmanTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+using HangmanGame;
+using NUnit.Framework;
+
+namespace HangmanTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var h = new Hangman("test");
+
+            var ret = h.Guess('a');
+
+            Assert.AreEqual("----", ret);
+
+            ret = h.Guess('t');
+
+            Assert.AreEqual("t--t", ret);
+
+            ret = h.Guess('e');
+
+            Assert.AreEqual("te-t", ret);
+
+            ret = h.Guess('s');
+
+            Assert.AreEqual("test", ret);
+
+        }
+    }
+}
