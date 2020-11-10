445f64244c09911b3940902f6557ed03b30fcb16
Benjamin.Klink@nexus-ag.de <Benjamin.Klink@nexus-ag.de>
2020-11-10 09:28:54 +0100

    bekl: Initial HangMan Kata Commit

---
+++ HangmanKata/Hangman.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HangmanKata
+{
+    public class Hangman
+    {
+        public string SearchedWord { get; set; }
+        public string GuessedWord { get; set; } = String.Empty;
+        public Hangman(string secret)
+        {
+            SearchedWord = secret;
+
+            for (int i = 0; i < SearchedWord.Length; i++)
+            {
+                GuessedWord = GuessedWord + "-";
+            }
+        }
+
+        public string Guess(char letter)
+        {
+            if(SearchedWord.Contains(letter))
+            {
+                char[] wordArray = SearchedWord.ToCharArray();
+                string pattern = letter.ToString().ToUpper();
+
+                List<int> posArray = new List<int>();
+
+                foreach (Match m in Regex.Matches(SearchedWord, pattern))
+                {
+                    posArray.Add(m.Index);
+                }
+
+                for (int i = 0; i < posArray.Count; i++)
+                {
+                    var guessedArray = GuessedWord.ToCharArray();
+                    guessedArray[posArray[i]] = letter;
+                    GuessedWord = new string(guessedArray);
+                }
+            }
+            else
+            {
+                return GuessedWord;
+            }
+
+            return GuessedWord;
+        }
+    }
+}
---
+++ HangmanKata/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hangman hangman = new Hangman("DEVELOPER");
+            Console.WriteLine("Type in your guess :)");
+            var guess = Console.ReadLine()[0];
+            hangman.Guess(guess);
+        }
+    }
+}
